df9865499a768d1c736177d6d52f177dd8bdc432
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-03 00:40:06 +0300

    Create Main.hx

---
+++ sample/Main.hx
@@ -0,0 +1,29 @@
+package;
+
+import hxraudio.RAudio;
+import hxraudio.Types;
+import sys.io.Thread;
+
+class Main
+{
+	public static function main():Void
+	{
+		// Initialization
+		RAudio.InitAudioDevice();
+
+		var music:Music = RAudio.LoadMusicStream("anothermedium.ogg");
+
+		RAudio.PlayMusicStream(music);
+
+		Thread.create(function()
+		{
+			RAudio.UpdateMusicStream(music);
+		});
+
+		Sys.sleep(20); // Wait 20 seconds until deinitialization
+		
+		// De-Initialization
+		RAudio.UnloadMusicStream(music);
+		RAudio.CloseAudioDevice();
+	}
+}
