327322eeb3bd143429b99de9df1654bf37756663
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-09-02 22:27:56 +0300

    Update RAudio.hx

--- hxraudio/RAudio.hx
+++ hxraudio/RAudio.hx
@@ -24,4 +24,50 @@ extern class RAudio
 	// Set master volume (listener)
 	@:native('SetMasterVolume')
 	static function IsAudioDeviceReady(volume:Single):Void;
+
+	// Wave/Sound loading/unloading functions
+
+	// Load wave data from file
+	@:native('LoadWave')
+	static function LoadWave(fileName:cpp.ConstCharStar):Wave;
+
+	// Load wave from memory buffer, fileType refers to extension: i.e. ".wav"
+	@:native('LoadWaveFromMemory')
+	static function LoadWaveFromMemory(fileType:cpp.ConstCharStar, fileData:cpp.RawConstPointer<cpp.UInt8>, dataSize:Int):Wave;
+
+	// Checks if wave data is ready
+	@:native('IsWaveReady')
+	static function IsWaveReady(wave:Wave):Bool;
+
+	// Load sound from file
+	@:native('LoadSound')
+	static function LoadSound(fileName:cpp.ConstCharStar):Sound;
+
+	// Load sound from wave data
+	@:native('LoadSoundFromWave')
+	static function LoadSoundFromWave(wave:Wave):Sound;
+
+	// Checks if a sound is ready
+	@:native('IsSoundReady')
+	static function IsSoundReady(sound:Sound):Bool;
+
+	// Update sound buffer with new data
+	@:native('UpdateSound')
+	static function UpdateSound(sound:Sound, data:cpp.RawConstPointer<cpp.Void>, samplesCount:Int):Void;
+
+	// Unload wave data
+	@:native('UnloadWave')
+	static function UnloadWave(wave:Wave):Void;
+
+	// Unload sound
+	@:native('UnloadSound')
+	static function UnloadSound(sound:Sound):Void;
+
+	// Export wave data to file, returns true on success
+	@:native('ExportWave')
+	static function ExportWave(wave:Wave, fileName:cpp.ConstCharStar):Bool;
+
+	// Export wave sample data to code (.h), returns true on success
+	@:native('ExportWaveAsCode')
+	static function ExportWaveAsCode(wave:Wave, fileName:cpp.ConstCharStar):Bool;
 }
