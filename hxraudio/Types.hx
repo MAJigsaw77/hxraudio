dce83ed7fc2099f9a5bef28d1430af69bdbee038
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-02-21 12:29:29 +0200

    Test

--- hxraudio/Types.hx
+++ hxraudio/Types.hx
@@ -8,21 +8,21 @@ class Types {} // blank
 
 typedef AudioCallback = cpp.Callable<(bufferData:cpp.RawPointer<cpp.Void>, frames:cpp.UInt32) -> Void>;
 
-// Wave, audio wave data
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
 @:structAccess
 @:native('Wave')
-extern class Wave
+private extern class WaveImpl
 {
 	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
 	var sampleRate:cpp.UInt32; // Frequency (samples per second)
 	var sampleSize:cpp.UInt32; // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
 	var channels:cpp.UInt32; // Number of channels (1-mono, 2-stereo, ...)
-
 	var data:cpp.RawPointer<cpp.Void>; // Buffer data pointer
 }
+
+typedef Wave = cpp.Struct<Wave>;
 
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
@@ -34,46 +34,47 @@ extern class RAudioBuffer {}
 @:native('rAudioProcessor')
 extern class RAudioProcessor {}
 
-// AudioStream, custom audio stream
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
 @:structAccess
 @:native('AudioStream')
-extern class AudioStream
+private extern class AudioStreamImpl
 {
 	var buffer:cpp.RawPointer<RAudioBuffer>; // Pointer to internal data used by the audio system
 	var processor:cpp.RawPointer<RAudioProcessor>; // Pointer to internal data processor, useful for audio effects
-
 	var sampleRate:cpp.UInt32; // Frequency (samples per second)
 	var sampleSize:cpp.UInt32; // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
 	var channels:cpp.UInt32; // Number of channels (1-mono, 2-stereo, ...)
 }
 
-// Sound
+typedef AudioStream = cpp.Struct<AudioStreamImpl>;
+
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
 @:structAccess
 @:native('Sound')
-extern class Sound
+private extern class SoundImpl
 {
 	var stream:AudioStream; // Audio stream
 	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
 }
 
-// Music, audio stream, anything longer than ~10 seconds should be streamed
+typedef Sound = cpp.Struct<SoundImpl>;
+
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
 @:structAccess
 @:native('Music')
-extern class Music
+private extern class MusicImpl
 {
 	var stream:AudioStream; // Audio stream
 	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
 	var looping:Bool; // Music looping enable
-
 	var ctxType:Int; // Type of music context (audio filetype)
 	var ctxData:cpp.RawPointer<cpp.Void>; // Audio context data, depends on type
 }
+
+typedef Music = cpp.Struct<MusicImpl>;
