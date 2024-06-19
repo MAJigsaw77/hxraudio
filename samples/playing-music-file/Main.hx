c551ecc05caa9d5498b94a710b05ffeb70ffcf1c
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-06-19 09:10:20 +0300

    Update Main.hx

--- samples/playing-music-file/Main.hx
+++ samples/playing-music-file/Main.hx
@@ -4,6 +4,28 @@ import hxraudio.RAudio;
 import hxraudio.Types;
 import sys.thread.Thread;
 
+@:headerInclude('stdarg.h')
+@:headerCode('
+#undef TRACELOG
+
+#define TRACELOG(level, ...) TraceLog(__VA_ARGS__)
+')
+@:headerNamespaceCode('
+static void TraceLog(const char *text, ...)
+{
+	va_list args;
+
+	va_start(args, text);
+
+	char buffer[256] = { 0 };
+
+	strcpy(buffer, text);
+	strcat(buffer, "\\n");
+
+	vprintf(buffer, args);
+
+	va_end(args);
+}')
 class Main
 {
 	public static function main():Void
