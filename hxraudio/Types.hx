d4ad4ce8ce207649bbe456b03dfe75f6e9502734
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2024-07-02 15:49:02 +0300

    The docs are now more visible

--- hxraudio/Types.hx
+++ hxraudio/Types.hx
@@ -1,13 +1,17 @@
 package hxraudio;
 
+/**
+ * Dummy class for importing RAudio types.
+ */
 #if !cpp
 #error 'RAudio supports only C++ target platforms.'
 #end
+class Types {}
 
-class Types {} // blank
-
+@:dox(hide)
 typedef AudioCallback = cpp.Callable<(bufferData:cpp.RawPointer<cpp.Void>, frames:cpp.UInt32) -> Void>;
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
@@ -15,23 +19,53 @@ typedef AudioCallback = cpp.Callable<(bufferData:cpp.RawPointer<cpp.Void>, frame
 @:native('Wave')
 extern class Wave
 {
-	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
-	var sampleRate:cpp.UInt32; // Frequency (samples per second)
-	var sampleSize:cpp.UInt32; // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
-	var channels:cpp.UInt32; // Number of channels (1-mono, 2-stereo, ...)
-	var data:cpp.RawPointer<cpp.Void>; // Buffer data pointer
+	/**
+	 * Allocates a new Wave object.
+	 *
+	 * @return A new Wave object.
+	 */
+	@:native('Wave')
+	static function alloc():Wave;
+
+	/**
+	 * Total number of frames (considering channels)
+	 */
+	var frameCount:cpp.UInt32;
+
+	/**
+	 * Frequency (samples per second)
+	 */
+	var sampleRate:cpp.UInt32;
+
+	/**
+	 * Bit depth (bits per sample): 8, 16, 32 (24 not supported)
+	 */
+	var sampleSize:cpp.UInt32;
+
+	/**
+	 * Number of channels (1-mono, 2-stereo, ...)
+	 */
+	var channels:cpp.UInt32;
+
+	/**
+	 * Buffer data pointer
+	 */
+	var data:cpp.RawPointer<cpp.Void>;
 }
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:native('rAudioBuffer')
 extern class RAudioBuffer {}
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:native('rAudioProcessor')
 extern class RAudioProcessor {}
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
@@ -39,13 +73,41 @@ extern class RAudioProcessor {}
 @:native('AudioStream')
 extern class AudioStream
 {
-	var buffer:cpp.RawPointer<RAudioBuffer>; // Pointer to internal data used by the audio system
-	var processor:cpp.RawPointer<RAudioProcessor>; // Pointer to internal data processor, useful for audio effects
-	var sampleRate:cpp.UInt32; // Frequency (samples per second)
-	var sampleSize:cpp.UInt32; // Bit depth (bits per sample): 8, 16, 32 (24 not supported)
-	var channels:cpp.UInt32; // Number of channels (1-mono, 2-stereo, ...)
+	/**
+	 * Allocates a new AudioStream object.
+	 *
+	 * @return A new AudioStream object.
+	 */
+	@:native('AudioStream')
+	static function alloc():AudioStream;
+
+	/**
+	 * Pointer to internal data used by the audio system
+	 */
+	var buffer:cpp.RawPointer<RAudioBuffer>;
+
+	/**
+	 * Pointer to internal data processor, useful for audio effects
+	 */
+	var processor:cpp.RawPointer<RAudioProcessor>;
+
+	/**
+	 * Frequency (samples per second)
+	 */
+	var sampleRate:cpp.UInt32;
+
+	/**
+	 * Bit depth (bits per sample): 8, 16, 32 (24 not supported)
+	 */
+	var sampleSize:cpp.UInt32;
+
+	/**
+	 * Number of channels (1-mono, 2-stereo, ...)
+	 */
+	var channels:cpp.UInt32;
 }
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
@@ -53,10 +115,26 @@ extern class AudioStream
 @:native('Sound')
 extern class Sound
 {
-	var stream:AudioStream; // Audio stream
-	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
+	/**
+	 * Allocates a new Sound object.
+	 *
+	 * @return A new Sound object.
+	 */
+	@:native('Sound')
+	static function alloc():Sound;
+
+	/**
+	 * Audio stream
+	 */
+	var stream:AudioStream;
+
+	/**
+	 * Total number of frames (considering channels)
+	 */
+	var frameCount:cpp.UInt32;
 }
 
+@:dox(hide)
 @:buildXml('<include name="${haxelib:hxraudio}/project/Build.xml" />')
 @:include('raudio.h')
 @:unreflective
@@ -64,9 +142,36 @@ extern class Sound
 @:native('Music')
 extern class Music
 {
-	var stream:AudioStream; // Audio stream
-	var frameCount:cpp.UInt32; // Total number of frames (considering channels)
-	var looping:Bool; // Music looping enable
-	var ctxType:Int; // Type of music context (audio filetype)
-	var ctxData:cpp.RawPointer<cpp.Void>; // Audio context data, depends on type
+	/**
+	 * Allocates a new Music object.
+	 *
+	 * @return A new Music object.
+	 */
+	@:native('Music')
+	static function alloc():Music;
+
+	/**
+	 * Audio stream
+	 */
+	var stream:AudioStream;
+
+	/**
+	 * Total number of frames (considering channels)
+	 */
+	var frameCount:cpp.UInt32;
+
+	/**
+	 * Music looping enable
+	 */
+	var looping:Bool;
+
+	/**
+	 * Type of music context (audio filetype)
+	 */
+	var ctxType:Int;
+
+	/**
+	 * Audio context data, depends on type
+	 */
+	var ctxData:cpp.RawPointer<cpp.Void>;
 }
